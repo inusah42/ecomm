8e8478f608670d3244cf440844b39499434b2b03
He Huanchen <xiaoyaohhc@gmail.com>
2019-10-14 21:29:04 -0400

    php modification

--- signup/signup.php
+++ signup/signup.php
@@ -12,5 +12,18 @@
     $state = $_POST['state'];
     $zip = $_POST['zip'];
 
-    $sqlinsert="insert into Users(Full_Name,Email,Username,Password,Address,City,State,Zip) values('{$fullname}','{$email}','{$username}','{$hashpass}','{$address}','{$city}','{$state}','{$zip}')";
+    
+    if(pg_fetch_array(pg_query($db_connection,"select * from Users where username = '$username'"))){
+        echo "<script>alert('username taken');window.location.href='index.html'</script>";
+    } else{
+        $sqlinsert="insert into Users(Full_Name,Email,Username,Password,Address,City,State,Zip) values('{$fullname}','{$email}','{$username}','{$hashpass}','{$address}','{$city}','{$state}','{$zip}')";
+ 
+        if(!(pg_query($sqlinsert))){
+            echo "<script>alert('failed');window.location.href='index.html'</script>";
+        }else{
+            echo "<script>alert('Success!'')</script>";
+        }
+    }
+
+    pg_connect($db_connection);
 ?>
