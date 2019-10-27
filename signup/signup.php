6e29b976df8873ad215cb738dfabfbb7e019d6aa
Diallo Inusah <inusahdiallo@yahoo.com>
2019-10-27 15:00:22 -0400

    added php mailer

--- signup/signup.php
+++ signup/signup.php
@@ -1,6 +1,11 @@
 <?php
-    $db_connection = pg_connect("host=ec2-174-129-41-127.compute-1.amazonaws.com dbname=dm2oitjf83u73 user=tjzntcroxpnjme password=f26ae870fba99aa4ec86ad28f5b4e2df70de121c81324e818122615da4dcb59a");
-
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP; 
+    require 'src/Exception.php';
+    require 'src/PHPMailer.php';
+    require 'src/SMTP.php';
+    $db_connection = pg_connect("host=ec2-54-83-55-125.compute-1.amazonaws.com dbname=d3h17gvrd6hlhs user=kpccbqhujjcixk password=c732048928370d49a64e4be8718393111f3a0508e07ca095eda8e7a3ade16110");
     
     $fullname = $_POST['name'];
     $email = $_POST['email'];
@@ -11,15 +16,36 @@
     $city = $_POST['city'];
     $state = $_POST['state'];
     $zip = $_POST['zip'];
-
+    $mail=new PHPMailer(true);
     
-    if(pg_fetch_array(pg_query($db_connection,"select * from Users where username = '$username'"))){
+    if(pg_fetch_array(pg_query($db_connection,"SELECT * FROM Users where username = '$username'"))){
         echo "<script>alert('username taken');window.location.href='index.html'</script>";
-    }else if(pg_fetch_array(pg_query($db_connection,"select * from Users where email = '$email'"))){
+    }else if(pg_fetch_array(pg_query($db_connection,"SELECT * FROM Users where email = '$email'"))){
         echo "<script>alert('email taken');window.location.href='index.html'</script>";
     } else{
-        $sqlinsert="insert into Users(Full_Name,Email,Username,Password,Address,City,State,Zip) values('$fullname','$email','$username','$hashpass','$address','$city','$state','$zip')";
- 
+        $sqlinsert="INSERT INTO table Users(Full_Name,Email,Username,Password,Address,City,State,Zip) values('$fullname','$email','$username','$hashpass','$address','$city','$state','$zip')";
+        try{
+            $mail->IsSMTP();
+            $mail->IsSMTP(); // enable SMTP
+            $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+            $mail->SMTPAuth = true; // authentication enabled
+            $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+            $mail->Host = "smtp.gmail.com";
+            $mail->Port = 587; // or 587
+            $mail->IsHTML(true);
+            $mail->Username = "boardhubzz@gmail.com";
+            $mail->Password = "boardhub478";
+            
+            $mail->SetFrom("boardhubzz@gmail.com");
+            $mail->Subject = "Account Creation";
+            $mail->Body = "Hello Your Account to BoardHub! has been Created!";
+            $mail->AddAddress($email);
+            $mail->Send();
+            #echo "Message has been sent!";
+        }
+        catch(Exception e){
+            echo "Mail Error: " . $mail->ErrorInfo;
+        }
         if(!(pg_query($sqlinsert))){
             echo "<script>alert('failed');window.location.href='index.html'</script>";
         }else{
