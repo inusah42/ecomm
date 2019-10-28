14345fa236fe67076f694956f398512038b0efd6
inusah42 <55960469+inusah42@users.noreply.github.com>
2019-10-28 04:05:53 -0400

    Update signup.php

--- signup/signup.php
+++ signup/signup.php
@@ -1,7 +1,7 @@
 <?php
-    #use PHPMailer\PHPMailer\PHPMailer;
-    #use PHPMailer\PHPMailer\Exception;
-    #use PHPMailer\PHPMailer\SMTP; 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP; 
     require '../src/Exception.php';
     require '../src/PHPMailer.php';
     require '../src/SMTP.php';
